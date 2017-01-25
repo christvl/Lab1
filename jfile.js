67c8803dd42e56a7a67de7351f85b27ad0c16115
christvl <trgrimesy@gmail.com>
2017-01-25 16:31:28 +0100

    Add files via upload

---
+++ jfile.js
@@ -0,0 +1,89 @@
+var table = document.getElementById("myTable");
+
+function gameBoard(){
+    for(var i = 0; i < 5; i++){
+        var tr = table.insertRow(i);
+        for(var j = 0; j < 5; j++){
+            var td = tr.insertCell(j).classList.add("lights-on");
+        }
+    }
+    
+    var arr = document.querySelectorAll("td");
+    for(var i = 0; i < arr.length; i++){
+        arr[i].addEventListener("click", game);
+    }
+}
+
+gameBoard();
+
+function game(){
+    var el = this;
+    var i = this.cellIndex;
+    
+    if(el.classList.contains("lights-on")){
+        el.classList.remove("lights-on");
+        el.classList.add("lights-off");  
+    } else{
+        el.classList.remove("lights-off");
+        el.classList.add("lights-on");  
+    }
+    if(el.previousElementSibling != null && el.previousElementSibling.classList.contains("lights-on")){
+        if(el.previousElementSibling != null){
+            el.previousElementSibling.classList.remove("lights-on");
+            el.previousElementSibling.classList.add("lights-off");
+        }
+    } else{
+        if(el.previousElementSibling != null){
+            el.previousElementSibling.classList.remove("lights-off");
+            el.previousElementSibling.classList.add("lights-on");
+        }
+    }
+    if(el.nextElementSibling != null && el.nextElementSibling.classList.contains("lights-on")){
+        if(el.nextElementSibling != null){
+            el.nextElementSibling.classList.remove("lights-on");
+            el.nextElementSibling.classList.add("lights-off");
+        }
+    } else{
+        if(el.nextElementSibling != null){
+            el.nextElementSibling.classList.remove("lights-off");
+            el.nextElementSibling.classList.add("lights-on");
+        }
+    }
+    
+    if(el.parentNode.previousElementSibling != null && el.parentNode.previousElementSibling.children[i].classList.contains("lights-on")){
+        if(el.parentNode.previousElementSibling != null){
+            el.parentNode.previousElementSibling.children[i].classList.remove("lights-on");
+            el.parentNode.previousElementSibling.children[i].classList.add("lights-off");
+        }
+    } else{
+        if(el.parentNode.previousElementSibling != null){
+            el.parentNode.previousElementSibling.children[i].classList.remove("lights-off");
+            el.parentNode.previousElementSibling.children[i].classList.add("lights-on");
+        }
+    }
+    
+    if(el.parentNode.nextElementSibling != null && el.parentNode.nextElementSibling.children[i].classList.contains("lights-on")){
+        if(el.parentNode.nextElementSibling != null){
+            el.parentNode.nextElementSibling.children[i].classList.remove("lights-on");
+            el.parentNode.nextElementSibling.children[i].classList.add("lights-off");
+        }
+    } else{
+        if(el.parentNode.nextElementSibling != null){
+            el.parentNode.nextElementSibling.children[i].classList.remove("lights-off");
+            el.parentNode.nextElementSibling.children[i].classList.add("lights-on");
+        }
+    }
+    
+    result();
+}
+
+function result(){
+    if(document.querySelectorAll(".lights-on").length === 0){
+        alert("Grattis! Du vann spelet!");
+        var cellClass = document.querySelectorAll("td");
+        for(var i = 0; i < cellClass.length; i++){
+            cellClass[i].classList.remove("lights-off");
+            cellClass[i].classList.add("lights-on");
+        }
+    }
+}
